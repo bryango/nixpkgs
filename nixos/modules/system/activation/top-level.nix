5f5fb2f83209e9c55e69ce064138f31f26e8c794
Alois Wohlschlager <alois1@gmx-topmail.de>
2023-09-24 20:52:50 +0200

    nixos/top-level: improve replaceRuntimeDependencies
    Instead of iterating over all replacements and applying them one by one,
    use the newly introduced replaceDependencies function to apply them all
    at once for replaceRuntimeDependencies. The advantages are twofold in
    case there are multiple replacements:
    * Performance is significantly improved, because there is only one pass
      over the closure to be made.
    * Correctness is improved, because replaceDependencies also replaces
      dependencies of the replacements themselves if applicable.

--- nixos/modules/system/activation/top-level.nix
+++ nixos/modules/system/activation/top-level.nix
@@ -68,9 +68,15 @@ let
     else showWarnings config.warnings baseSystem;
 
   # Replace runtime dependencies
-  system = foldr ({ oldDependency, newDependency }: drv:
-      pkgs.replaceDependency { inherit oldDependency newDependency drv; }
-    ) baseSystemAssertWarn config.system.replaceRuntimeDependencies;
+  system = let replacements = config.system.replaceRuntimeDependencies; in
+    if replacements == [] then
+      # Avoid IFD if possible, by sidestepping replaceDependencies if no replacements are specified.
+      baseSystemAssertWarn
+    else
+      pkgs.replaceDependencies {
+        drv = baseSystemAssertWarn;
+        inherit replacements;
+      };
 
   systemWithBuildDeps = system.overrideAttrs (o: {
     systemBuildClosure = pkgs.closureInfo { rootPaths = [ system.drvPath ]; };
