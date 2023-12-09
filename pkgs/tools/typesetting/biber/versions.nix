007b025cb3ddc1281d5f6547b13aefbeb10383e8
Bryan Lai <bryanlais@gmail.com>
2023-12-09 16:20:18 +0800

    biberVersions.biber-for-tectonic: init at 2.17
    Introduce alternative `biberVersions`. This is useful for packages such
    as `tectonic`, which depends on biber versions older than the current
    one in the nixpkgs texlive bundle.
    
    For biber<=2.17 on perl>=5.36.0 a patch is necessary.
    This is recovered from a previous nixpkgs commit:
    
      https://github.com/NixOS/nixpkgs/commit/c784cdbf6b98e3e3aefb678b6ee8309cbb8bee15
    
    Also, add @bryango as maintainer.
    
    Co-authored-by: Mauricio Collares <mauricio@collares.org>

---
+++ pkgs/tools/typesetting/biber/versions.nix
@@ -0,0 +1,30 @@
+{ lib, callPackage, fetchFromGitHub, fetchpatch, biber }:
+
+{
+  biber-from-texlive = biber;
+  biber-for-tectonic = let version = "2.17"; in (
+    biber.override {
+      ## this is necessary, otherwise wrong `meta.name`
+      texlive.pkgs.biber.texsource = {
+        inherit version;
+        inherit (biber) pname meta;
+      };
+    }
+  ).overrideAttrs (prevAttrs: {
+    src = fetchFromGitHub {
+      owner = "plk";
+      repo = "biber";
+      rev = "v${version}";
+      hash = "sha256-Tt2sN2b2NGxcWyZDj5uXNGC8phJwFRiyH72n3yhFCi0=";
+    };
+    patches = [
+      (fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/plk/biber/pull/411.patch";
+        hash = "sha256-osgldRVfe3jnMSOMnAMQSB0Ymc1s7J6KtM2ig3c93SE=";
+      })
+    ];
+    meta = prevAttrs.meta // {
+      maintainers = with lib.maintainers; [ bryango ];
+    };
+  });
+}
