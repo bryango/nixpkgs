ce2aee01405aa185618d80dde94d3033250a6773
Bryan Lai <bryanlais@gmail.com>
2023-12-10 12:35:48 +0800

    tectonic-with-biber: init wrapper
    This is a simple `symlinkJoin` wrapper that bundles tectonic with an
    appropriate version of biber. This is partially is recovered from:
    
      https://github.com/NixOS/nixpkgs/commit/5aa8e9f0f90b0c9bbdf7b18ead18704d1622c509
    
    Also, add @bryango as maintainer.
    
    Co-authored-by: Doron Behar <doron.behar@gmail.com>

---
+++ pkgs/tools/typesetting/tectonic/wrapper.nix
@@ -0,0 +1,53 @@
+{ lib
+, makeBinaryWrapper
+, symlinkJoin
+, tectonic
+, biberVersions
+, biber
+}:
+
+let
+
+  pname = "tectonic-with-biber";
+  inherit (biberVersions) biber-for-tectonic;
+  inherit (biber-for-tectonic) version;
+
+  # manually construct `name` for `symlinkJoin`
+  name = "${pname}-${version}";
+
+  meta = tectonic.meta // {
+    inherit name;
+    description = "Tectonic, wrapped with the correct biber version";
+    longDescription = ''
+      This package wraps tectonic with biber without triggering rebuilds.
+      The biber executable is exposed with a version suffix, such as
+      `biber-${version}`, to prevent conflict with the `biber` bundled with
+      texlive in nixpkgs.
+
+      This serves as a downstream fix for:
+      - https://github.com/tectonic-typesetting/tectonic/issues/893
+    '';
+    maintainers = with lib.maintainers; [ bryango ];
+  };
+
+in
+
+symlinkJoin {
+
+  inherit pname version name meta;
+
+  paths = [ tectonic ];
+  nativeBuildInputs = [ makeBinaryWrapper ];
+  passthru = { inherit biber; };
+
+  # tectonic runs biber when it detects it needs to run it, see:
+  # https://github.com/tectonic-typesetting/tectonic/releases/tag/tectonic%400.7.0
+  postBuild = ''
+    wrapProgram $out/bin/tectonic \
+      --prefix PATH : "${lib.getBin biber-for-tectonic}/bin"
+    makeBinaryWrapper "${lib.getBin biber-for-tectonic}/bin/biber" \
+      $out/bin/biber-${biber-for-tectonic.version}
+  '';
+  # the biber executable is exposed as `biber-${biber.version}`
+
+}
